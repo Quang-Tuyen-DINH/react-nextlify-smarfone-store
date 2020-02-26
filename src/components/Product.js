4f8858fd220eb301edfca1a07d9ac52bdde0e985
Quang Tuyen DINH <qtuyen43@gmail.com>
2020-02-26 23:42:22 +0100

    Getting used to JSX coding style

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import styled from'styled-components';
-import {Link} from 'react-router-dom';
-import {ProductConsumer} from '../context';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
   render() {
-    const {id, title, img, price, inCart} = this.props.product;
-    console.log('Here !!! ' + JSON.stringify(this.props.product));
+    const { id, title, img, price, inCart } = this.props.product;
 
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div className="img-container p-5">
+          <div
+            className="img-container p-5"
+            onClick={() => { }}
+          >
             <Link to="/details">
-              <img src={img} alt="product" className="card-img-top"/>
+              <img src={img} alt="product" className="card-img-top" />
             </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => { console.log('added to cart'); }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>{" "}in Cart</p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
