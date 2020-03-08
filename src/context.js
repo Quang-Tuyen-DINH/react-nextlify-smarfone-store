561b70e9a47a8c2eb2189a1aedb52a198c93aee5
Quang Tuyen DINH <qtuyen43@gmail.com>
2020-03-08 23:12:55 +0100

    Modify Product and Context components to show the exact product

--- src/context.js
+++ src/context.js
@@ -9,10 +9,10 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct
-  }
+  };
   componentDidMount() {
     this.setProducts();
-  }
+  };
   setProducts = () => {
     let tempProducts = [];
     storeProducts.forEach(item => {
@@ -22,13 +22,20 @@ class ProductProvider extends Component {
     this.setState( () => {
       return {products: tempProducts};
     })
-  }
-  handleDetail = () => {
-    console.log('hello from detail');
-  }
+  };
+  getItem = (id) => {
+    const product = this.state.products.find(item =>item.id === id);
+    return product;
+  };
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return {detailProduct: product}
+    })
+  };
   addToCart = (id) => {
     console.log(`Added product.s id is: ${id}`);
-  }
+  };
 
   render() {
     return (
