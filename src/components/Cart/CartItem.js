fab8f3692e954719b6f9ca9db3b18f9577d1752f
Quang Tuyen DINH <qtuyen43@gmail.com>
2020-03-20 00:09:17 +0100

    Advancing cart component on subTotal

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,15 +4,15 @@ export default function CartItem({item, value}) {
   const {id, title, img, price, total, count} = item;
   const {increment, decrement, removeItem} = value;
   return (
-    <div className="row my-1 text-capitalize text-center">
+    <div className="row my-4 text-capitalize text-center">
       <div className="col-10 mx-auto col-lg-2">
         <img src={img} style={{width:'5rem', height:'5rem'}} className="img-fluid" alt="product" />
       </div>
-      <div className="col-10 mx-auto col-lg-2">
+      <div className="col-10 mx-auto col-lg-2" style={{'font-size':'1.2rem'}}>
         <span className="d-lg-none">product : </span>
         {title}
       </div>
-      <div className="col-10 mx-auto col-lg-2">
+      <div className="col-10 mx-auto col-lg-2" style={{'font-size':'1.2rem'}}>
         <span className="d-lg-none">price : </span>
         {price}
       </div>
@@ -20,10 +20,19 @@ export default function CartItem({item, value}) {
         <div className="d-flex justify-content-center">
           <div>
             <span className="btn btn-cart mx-1" onClick={() => decrement(id)}>-</span>
-            <span className="btn btn-cart mx-1" onClick={() => removeItem(id)}>{count}</span>
+            <span className="btn mx-1">{count}</span>
             <span className="btn btn-cart mx-1" onClick={() => increment(id)}>+</span>
           </div>
         </div>
+      </div>
+      {/*  */}
+      <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon" onClick={() => removeItem(id)} style={{'font-size':'1.5rem'}}>
+          <i className="fas fa-trash"></i>
+        </div>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <strong>total item : € {total}</strong>
       </div>
     </div>
   )
